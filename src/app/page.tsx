9f174d3ba4248c91ee8891e3caf53e620095af8e
Ricardo Ramos <ricardoramos@Ricardos-MacBook-Air.local>
2023-03-06 18:19:13 -0800

    added elements for user input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,50 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client"
+import Image from "next/image"
+import { Inter } from "next/font/google"
+import styles from "./page.module.css"
+import { useState } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [workHistory, setWorkHistory] = useState<string>("")
+	const [jobDescription, setJobDescription] = useState<string>("")
+	const [content, setContent] = useState<string>("")
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+	const handlePaste = (event: React.ClipboardEvent<HTMLTextAreaElement>) => {
+		event.preventDefault()
+		const clipboardData = event.clipboardData || window.Clipboard.toString()
+		const pastedData = clipboardData.getData("text/html")
+		setWorkHistory(pastedData)
+	}
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className="main-container">
+			<div className="work-history-container">
+				<label htmlFor="test">Work History</label>
+				<textarea
+					name="test"
+					id="test"
+					cols={50}
+					rows={10}
+					// onPaste={handlePaste}
+					value={workHistory}
+					placeholder="Paste your work history here"
+					onChange={(e) => setWorkHistory(e.target.value)}
+				></textarea>
+			</div>
+			<div className="job-description-container">
+				<label htmlFor="job-description">Job Description</label>
+				<textarea
+					name="job-description"
+					id="job-description"
+					cols={50}
+					rows={10}
+					// onPaste={handlePaste}
+					value={jobDescription}
+					onChange={(e) => setJobDescription(e.target.value)}
+				></textarea>
+			</div>
+		</main>
+	)
 }
